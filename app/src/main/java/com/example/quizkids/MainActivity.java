4cfc75a7407995b57cb9d3fa45a90abacbad93c6
JYI18 <jenice3163@gmail.com>
2024-09-26 10:08:12 +0800

    Home page initial done

--- app/src/main/java/com/example/quizkids/MainActivity.java
+++ app/src/main/java/com/example/quizkids/MainActivity.java
@@ -1,24 +1,44 @@
 package com.example.quizkids;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.quizkids.Activities.SetsActivity;
+
 public class MainActivity extends AppCompatActivity {
+
+    CardView science,mathematics;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        //getSupportActionBar().hide();
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        science = findViewById(R.id.science);
+        mathematics = findViewById(R.id.mathematics);
+
+        science.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SetsActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
